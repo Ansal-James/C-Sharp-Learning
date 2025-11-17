0abbc501d4609fb16a779726f70a427cc8eddca5
Ansal James <ansaljames45@gmail.com>
2025-11-17 20:21:48 +0530

    Add files via upload

---
+++ DimondPattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Pattern
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for(int j = 5; j >= 1; j--)
+            {
+                if(i  >= j)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        for (int i = 4; i >= 1; i--)
+        {
+            Console.Write(' ');
+            for (int j = 4; j >= 1; j--)
+            {
+                if (i >= j)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
